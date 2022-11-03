9555148600bd04efd4e36003c9bc1836c83315c4
Davis Tran <austinjoey@ymail.com>
2022-11-02 22:36:18 -0400

    update

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ const PORT = process.env.PORT || 5000;
 
 const start = async () => {
     try {
+      console.log('test consoles');
       await connectDB(process.env.MONGO_URI)
       app.listen(PORT, () =>
         console.log(`Server is listening on port ${PORT}...`)
