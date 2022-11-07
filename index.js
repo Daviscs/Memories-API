7217a64000ebbb9b68e6d6ba570f55d566e1cba0
Davis Tran <austinjoey@ymail.com>
2022-11-07 18:52:39 -0500

    Updated Page limit

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ const PORT = process.env.PORT || 5000;
 
 const start = async () => {
     try {
-      console.log('test consoles');
       await connectDB(process.env.MONGO_URI)
       app.listen(PORT, () =>
         console.log(`Server is listening on port ${PORT}...`)
